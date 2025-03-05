4841fd99bb0f595a60ec9663a54e45eee9a61251
Sangjin You <rangjin2037@gmail.com>
2025-03-06 01:10:58 +0900

    Topic 이름을 application.yml 로 이동

--- central/src/main/kotlin/com/rangjin/central/global/kafka/KafkaProducerService.kt
+++ central/src/main/kotlin/com/rangjin/central/global/kafka/KafkaProducerService.kt
@@ -1,20 +1,23 @@
 package com.rangjin.central.global.kafka
 
 import com.rangjin.core.article.dto.CrawlingResultDto
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class KafkaProducerService (
+
+    @Value("\${spring.kafka.producer.topic}")
+    private val topicName: String,
 
     private val kafkaTemplate: KafkaTemplate<String, CrawlingResultDto>
 
 ) {
 
-    private final val topicName = "crawling-result"
-
     fun send(crawlingResultDto: CrawlingResultDto) {
-        kafkaTemplate.send(topicName, crawlingResultDto)
+        kafkaTemplate.send(topicName, UUID.randomUUID().toString(), crawlingResultDto)
     }
 
 }
